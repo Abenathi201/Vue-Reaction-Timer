66183bb0360eb233c5a07f4e318dcafd7a6a36e1
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-06-24 17:02:29 +0200

    Reaction timer

---
+++ src/components/BLock.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="block" v-if="showBlock" @click="endTimer">
+        Click Me
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['delay'],
+
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+
+        endTimer() {
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+        }
+    },
+}
+</script>
+
+<style>
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        background: #d9d9d9;
+        color: #000;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,35 @@
+<template>
+    <p class="text"> Reaction Time: {{ score }} m/s </p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+
+    data() {
+        return {
+            rank: null
+        }
+    },
+
+    mounted() {
+        if(this.score < 250) {
+            this.rank = "Well Done Friend!!!!"
+        }
+        else if(this.score < 400) {
+            this.rank = "Not Too Bad Buddy!!!!"
+        }
+        else{
+            this.rank = "BOOO!!! You Suck Guy"
+        }
+    },
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 20px;
+        font-weight: bold;
+    }
+</style>
